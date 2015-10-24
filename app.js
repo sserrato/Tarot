970fe7b4546624716cd93cabee001b3d83c3eaff
Sergio Serrato <serrato@cal.berkeley.edu>
2015-10-24 12:05:03 -0700

    comments to detail functions.

--- app.js
+++ app.js
@@ -22,11 +22,18 @@ var deck =
 ["the House of the Rising Sun","The Sun","XIX"],
 ["The year zero","Judgment","X"],
 ["The multiverse","The World","XI"]]
+//cards to be dealt in the Celtic Cross
 
-
-
-
-
+// Card 1:   The present
+// Card 2:   The immediate challenge facing the querent. You will often pull a difficult card here, which will indicate an obstacle that must be overcome. When you pull a "good" card here, examine it carefully because it will still represent a challenge.
+// Card 3:   Distant past, foundation. This card should indicate the root of the subject matter of the question
+// Card 4:   More recent past, including events. This will indicate events taking place, not necessarily directly connected to the question. For example, if a love affair going wrong Card 3 would show the root of why it is going wrong, whereas Card 4 will show something that recently happened to reflect this. You could see this as a "check comment" card - a way of seeing that the reading is sound.
+// Card 5:   The best that can be achieved. This is directly related to the question. Note that this may not necessarily gel against Card 10 - it depends whether you are able to get the best. However, a negative card here probably means that it is worth cutting your losses rather than putting any more effort into the situation.
+// Card 6: Immediate Future. This indicates events in the next few days or week(s). This reading does not cover months.
+// Card 7: Factors or inner feelings affecting the situation. Compare this against Card 1 in order to understand underlying forces/trends. If there is conflict between them this tends to indicate that the querent is going in the wrong direction.
+// Card 8: External influences. People, energies or events which will affect the outcome of the question and are beyond the querent's control.
+// Card 9: Hopes or fears around the situation. This may produce a card that confuses us badly. Always bear in mind that hopes and fears are closely intertwined, therefore that which we hope for may also be that which we fear, and so may fail to happen. Sometimes it is useful to draw a second card for clarification after the reading has been laid, and to read the two together.
+// Card 10: Final outcome. This is a fairly self explanatory card. However it is worth saying that if the card comes up somewhat ambiguous, once again it may be worth drawing three extra cards to clarify. These should be interpreted through the lens of Card 10. So if the card drawn is the Tower and we draw the Ace of Cups, Princess of Cups and Four of Cups in a reading about a relationship, the Tower would indicate that it is time to move on. The Ace of Cup suggests that a new relationship is promised with better things ahead. The Princess of Cups brings new love (and possibly pregnancy). The Four of Cups shows deep levels of contentment and happiness, and many options for progress.
 var card1 = "";
 var card2 = "";
 var card3 = "";
@@ -51,7 +58,7 @@ function dealCards(){
       card2 = (Math.floor((Math.random() * 22) + 1));
   //    $('#four').append('<div>The past:<br> ' + deck[card2] +'</div');
       pushDealtDeck(deck[card2], card2);
-      console.log("The past: "+  deck[card2] + card2);
+      console.log("The past: " +  deck[card2] + card2);
     }
     else {
     //  $('#four').append('<div>The past:<br> ' + deck[card2] +'</div');
@@ -107,9 +114,10 @@ var clickCounter = 0;
 
 // major arcana
 // 56 minor arana, 4 suits
+// this shows the card
 function  showCard(){
   if (clickCounter == 0) {
-    $("#five").append('<div id="card"> The present:<br> ' + deck[card1][1] + '</div');
+    $('#five').append('<div id="card"> The present:<br> ' + deck[card1][1] + '</div');
     clickCounter= clickCounter +1;
   }
   else if (clickCounter == 2) {
